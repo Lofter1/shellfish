3a7d535d3d41ec161f9ee1c8e1f4852ddba11116
Justin Jürs <justin099@web.de>
2021-03-06 17:18:03 +0100

    Set prototype for basic detail view

--- src/DesktopApp/ViewModels/SlideDetailsViewModel.cs
+++ src/DesktopApp/ViewModels/SlideDetailsViewModel.cs
@@ -1,9 +1,32 @@
-﻿using DesktopApp.Models;
+﻿using System;
+using System.Collections.Generic;
+using DesktopApp.Models;
+using ReactiveUI;
 
 namespace DesktopApp.ViewModels
 {
     public class SlideDetailsViewModel : DetailsViewModelBase<Slide>
     {
-        
+        public SlideDetailsViewModel()
+        {
+            Asset = new Slide
+            {
+                Title = "Test title",
+                Event = "DefCon",
+                Speaker = new Person
+                {
+                    FirstName = "Someone", 
+                    LastName = "Famous", 
+                    Website = new Uri("http://www.someonefamous.com")
+                },
+                FileType = "pdf",
+                Url = new Uri("/path/to/file.pdf"),
+                Tags = new List<Tag>
+                {
+                    new() {Name = "DefCon"}, 
+                    new() {Name = "Someone famous wrote this"}
+                }
+            };
+        }
     }
 }
