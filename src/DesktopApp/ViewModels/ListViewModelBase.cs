87ff661c324f4466828c816f7bea8956fe0b3887
Marlin Pagendarm <marlin@pagendarm.eu>
2021-03-19 00:47:10 +0100

    Add open button for main ui

--- src/DesktopApp/ViewModels/ListViewModelBase.cs
+++ src/DesktopApp/ViewModels/ListViewModelBase.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Reactive;
 using DesktopApp.Models;
 using ReactiveUI;
@@ -10,11 +12,25 @@ namespace DesktopApp.ViewModels
         public ObservableCollection<T> Assets { get; set; }
         public ReactiveCommand<Unit, Unit> AddAssetCommand => ReactiveCommand.Create(AddAsset);
         public ReactiveCommand<Unit, Unit> OpenAssetDetailsCommand => ReactiveCommand.Create(OpenAssetDetails);
+        public ReactiveCommand<Unit, Unit> OpenAssetCommand => ReactiveCommand.Create(OpenAsset);
 
         public T SelectedAsset { set; get; }
         
         
         protected abstract void OpenAssetDetails();
         protected abstract void AddAsset();
+
+        private void OpenAsset()
+        {
+            switch (Environment.OSVersion.Platform)
+            {
+                case PlatformID.Win32NT:
+                    Process.Start(SelectedAsset.Url.ToString());
+                    break;
+                case PlatformID.Unix:
+                    Process.Start("xdg-open", SelectedAsset.Url.ToString());
+                    break;
+            }
+        }
     }
 }
