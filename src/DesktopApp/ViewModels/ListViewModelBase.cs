7ebf9eda0bca920871d55412c7bf8d04da333359
olly <ollyschulz@protonmail.com>
2021-03-19 00:36:46 +0100

    add delete funcionality in ListViewModel.

--- src/DesktopApp/ViewModels/ListViewModelBase.cs
+++ src/DesktopApp/ViewModels/ListViewModelBase.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.ObjectModel;
 using System.Reactive;
+using DesktopApp.DataAccess;
 using DesktopApp.Models;
 using ReactiveUI;
 
@@ -10,11 +11,21 @@ namespace DesktopApp.ViewModels
         public ObservableCollection<T> Assets { get; set; }
         public ReactiveCommand<Unit, Unit> AddAssetCommand => ReactiveCommand.Create(AddAsset);
         public ReactiveCommand<Unit, Unit> OpenAssetDetailsCommand => ReactiveCommand.Create(OpenAssetDetails);
+        
+        public ReactiveCommand<Unit, Unit> DeleteAssetCommand => ReactiveCommand.Create(DeleteAsset);
 
         public T SelectedAsset { set; get; }
         
         
         protected abstract void OpenAssetDetails();
         protected abstract void AddAsset();
+
+        public void DeleteAsset()
+        {
+            using var dbContext = new ShellFishDbContext();
+            dbContext.Remove(SelectedAsset);
+            dbContext.SaveChanges();
+            Assets.Remove(SelectedAsset);
+        }
     }
 }
