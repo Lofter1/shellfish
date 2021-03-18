e7975d7cbfccba9569f5f453d686b5624610ae1b
olly <ollyschulz@protonmail.com>
2021-03-19 00:08:22 +0100

    add ListView

---
+++ src/DesktopApp/Views/ListView.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DesktopApp.Views
+{
+    public class ListView : UserControl
+    {
+        public ListView()
+        {
+            InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
